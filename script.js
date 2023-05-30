e417829d9d51e98dd680f3cc1cb0e89f1fc94a26
David <trungdavid969@gmail.com>
2023-05-30 18:14:12 +0200

    add detail page and its styling

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const log = document.querySelector(".login")
 const reg = document.querySelector(".registrera")
 
-const createBtn = document.querySelector(".createbutton")
-createBtn.addEventListener("click", function () {
+const createBtn = document.querySelector(".createbutton"); 
+
+
+createBtn?.addEventListener("click", function () {
     log.style.display = "none"
     reg.style.display = "block"
 });
